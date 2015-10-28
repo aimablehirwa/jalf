189327109c4b00661a4f52bea4cf26f74360c9a7
Bernard Lambeau <blambeau@gmail.com>
2015-10-28 15:51:26 +0100

    Ping: Add test to union operator

---
+++ src/test/java/jalf/dsl/UnionTest.java
@@ -0,0 +1,32 @@
+package jalf.dsl;
+
+import jalf.Relation;
+
+import org.junit.Test;
+
+import static jalf.DSL.*;
+import static jalf.fixtures.SuppliersAndParts.*;
+import static org.junit.Assert.*;
+
+public class UnionTest {
+
+    @Test
+    public void testItWorksAsExpected() {
+        Relation left = relation(
+                tuple(SID, "S1"),
+                tuple(SID, "S2")
+        );
+        Relation right = relation(
+                tuple(SID, "S2"),
+                tuple(SID, "S3")
+        );
+        Relation expected = relation(
+                tuple(SID, "S1"),
+                tuple(SID, "S2"),
+                tuple(SID, "S3")
+        );
+        Relation actual = union(left, right);
+        assertEquals(expected, actual);
+    }
+
+}
